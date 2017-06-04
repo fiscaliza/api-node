b4c53ee9cd4b3ecaf6bc59c8a7b2102727ebb085
Araan Branco <araan666@gmail.com>
2017-06-03 23:06:13 -0300

    Finish api simple

--- controllers/bidding.js
+++ controllers/bidding.js
@@ -4,18 +4,20 @@ export const fetch = async(req, res, next, id) => {
   const { Bidding } = db.connect('luppa').connection.models
   req.entities = req.entities || {}
 
-  let document = await Bidding.findOne({ _id: id })
-  req.entities.item = document
+  const a = new Bidding({ name: 'All' })
+
+  await a.save()
+
+  req.entities.item = await Bidding.findOne({ _id: id })
   next()
 }
 
 export const findOne = async(req, res) => {
-  const { Bidding } = db.connect('luppa').connection.models
-
   if (req.entities) {
-    res.status(200).json(req.entities.item)
+    console.log(req.entities)
+    res.status(200).json({ item: req.entities.item })
   } else {
-    res.status(404).json({ error: 'Item not found '})
+    res.status(404).json({ error: 'Item not found' })
   }
   res.end()
 }
@@ -23,8 +25,8 @@ export const findOne = async(req, res) => {
 export const getAll = async(req, res) => {
   const { Bidding } = db.connect('luppa').connection.models
 
-  const documents = await Bidding.find({}, 'productName score orderType').sort('score')
-
+  const documents = await Bidding.find({}, 'productAlias score _id orderType').sort('score')
+  console.log(documents)
   res.status(200).json(documents)
   res.end()
 }
